1d4d78dea22924447a2d84e2adce92785b1c0cb2
plcheaters <gurthylurthy@proton.me>
2025-07-13 13:31:47 +0100

    Update Showcase.js

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,35 +1,42 @@
-import Link from "next/link";
 import { motion } from "framer-motion";
 
 const Showcase = () => {
   return (
     <motion.div
-      initial={{
-        opacity: 0,
-      }}
+      initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
     >
       <main className="showcase">
         <h1 className="showcase-header h-color">BOT</h1>
         <h2 className="showcase-header-text h-color">
-          Dont let one mistake ruin your Discord server.
+          Don't let one mistake ruin your Discord server.
         </h2>
         <p className="showcase-para p-color">
-          Revert has it all, anti-links, anti-invite-links, antispam, etc.
+          Revert has it all: anti-links, anti-invite-links, anti-spam, etc.
         </p>
 
         <span className="p-color">Revert is online (99.99%)</span>
         <div className="showcase-btn">
           <button className="btn">
-            <Link className="h-color no-decoration" href="#">
+            <a
+              className="h-color no-decoration"
+              href="https://discord.com/oauth2/authorize?client_id=1323812197750538331&permissions=8&integration_type=0&scope=bot"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Add Bot
-            </Link>
+            </a>
           </button>
 
           <button className="btn">
-            <Link className="h-color no-decoration" href="#">
+            <a
+              className="h-color no-decoration"
+              href="https://discord.gg/S3xCWWHsNr"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Support Server
-            </Link>
+            </a>
           </button>
         </div>
       </main>
