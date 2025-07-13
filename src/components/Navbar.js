7d1fe71d593b714db7ca7901cf9fc1ee8593fef6
plcheaters <gurthylurthy@proton.me>
2025-07-13 13:26:47 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,48 +2,41 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>
-      <navbar className="navbar">
-        <nav className="navbar-header">
-          <ul>
-            <li>
-              <Link className="h-color" href="/">
-                Revert
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <nav className="navbar-items">
-          <ul>
-            <li>
-              <Link className="p-color" href="/">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link className="p-color" href="/commands">
-                Commands
-              </Link>
-            </li>
-            <li>
-              <a
-                className="p-color"
-                href="https://discord.com/oauth2/authorize?client_id=1323812197750538331&permissions=8&integration_type=0&scope=bot"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Add Bot
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </navbar>
+    <div className="navbar">
+      <nav className="navbar-header">
+        <ul>
+          <li>
+            <Link className="h-color" href="/">
+              Revert
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <nav className="navbar-items">
+        <ul>
+          <li>
+            <Link className="p-color" href="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="p-color" href="/commands">
+              Commands
+            </Link>
+          </li>
+          <li>
+            <a
+              className="p-color"
+              href="https://discord.com/oauth2/authorize?client_id=1323812197750538331&permissions=8&integration_type=0&scope=bot"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Add Bot
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
-  );
-};
-
-export default Navbar;
-
   );
 };
 
